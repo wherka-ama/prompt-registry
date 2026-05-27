01de9734b8e615cd9618d4691d0aaecdab089e28
Waldek Herka <87032474+wherka-ama@users.noreply.github.com>
2026-05-27 17:46:42 +0200

    fix(migration): simplify infra barrel exports
    - Simplify packages/infra/src/index.ts to only export github and utilities
    - Subdirectories don't have index.ts files in source branch
    - Infra package builds successfully

--- packages/infra/src/index.ts
+++ packages/infra/src/index.ts
@@ -7,36 +7,6 @@
 // GitHub
 export * from './github';
 
-// Harvest
-export * from './harvest';
-
-// Search
-export * from './search';
-
-// Stores
-export * from './stores';
-
-// Writers
-export * from './writers';
-
-// Resolvers
-export * from './resolvers';
-
-// Discovery
-export * from './discovery';
-
-// Downloaders
-export * from './downloaders';
-
-// Extractors
-export * from './extractors';
-
-// Filesystem
-export * from './fs';
-
-// HTTP
-export * from './http';
-
 // Utilities
 export * from './checksum';
 export * from './default-hubs';
